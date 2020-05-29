d03bdd5eb0ce28c3b0b0be0f0441be87883f274c
natsuki.iwashita <secret.love.2243@gmail.com>
2020-05-29 13:54:37 +0000

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_034238) do
+ActiveRecord::Schema.define(version: 2020_05_29_112019) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
